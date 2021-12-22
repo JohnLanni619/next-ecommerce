e3073d2c8691091101563ed969d5e3c43205030a
John Lanni <Johnlanni619@gmail.com>
2021-12-22 15:04:49 -0500

    added price and name to products page/added styling

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -25,7 +25,7 @@ const HeaderStyles = styled.header`
         display: grid;
         grid-template-columns: auto 1fr;
         justify-content: space-between;
-        align-items: center;
+        align-items: stretch;
     }
 
     .sub-bar {
@@ -47,11 +47,12 @@ export default function Header() {
                 <Logo>
                     <Link href="/"> Demo Time</Link>
                 </Logo>
+                <Nav/>
             </div>
             <div className="sub-bar">
                 <p>Search</p>
             </div>
-            <Nav/>
+            
         </HeaderStyles>
     );
 }
--- frontend/components/Product.js
+++ frontend/components/Product.js
@@ -1,3 +1,25 @@
+import ItemStyles from './styles/ItemStyles';
+import Title from './styles/Title';
+import PriceTag from './styles/PriceTag';
+import Link from 'next/link';
+import formatMoney from '../lib/formatMoney';
+
 export default function Product({product}) {
-    return <p>{product.name}</p>
+    return(
+        <ItemStyles>
+            <img 
+                src={product?.photo?.image?.
+                publicUrlTransformed} 
+                alt={product.name}
+            />
+            <Title>
+                <Link href={`/product/${product.id}`}>
+                    {product.name}
+                </Link>
+            </Title>
+            <PriceTag>{formatMoney(product.price)}</PriceTag>
+            <p>{product.description}</p>
+            {/* TODO: Add buttons to add and remove items */}
+        </ItemStyles>
+    )
 }
